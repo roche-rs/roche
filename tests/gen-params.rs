3134d15bee2c97b70cfc0eccf95e68cb415087eb
Anthony Whalley <anton@venshare.com>
2020-12-30 01:21:06 +0000

    split gen test

--- tests/gen-params.rs
+++ tests/gen-params.rs
@@ -1,44 +1,13 @@
 use remove_dir_all::*;
-use std::env;
-use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::sync::atomic::*;
+use std::{env, fs, thread, time};
 
 #[cfg(test)]
 
 static CNT: AtomicUsize = AtomicUsize::new(0);
 thread_local!(static IDX: usize = CNT.fetch_add(1, Ordering::SeqCst));
-
-#[test]
-fn generate_docker_file_default() {
-    let path = root("generate_docker_file_default");
-    if Path::new(&path).exists() {
-        remove_dir_all(&path).unwrap();
-    }
-    fs::create_dir_all(&path).expect(&format!("couldn't create {:?} directory", path.display()));
-    assert!(env::set_current_dir(&path).is_ok());
-
-    let mut me = env::current_exe().expect("couldn't find current exe");
-    me.pop(); // chop off exe name
-    me.pop(); // chop off `deps`
-    me.push("roche");
-
-    let mut init_cmd = Command::new(&me)
-        .arg("gen")
-        .current_dir(&path)
-        .spawn()
-        .unwrap();
-    assert!(init_cmd.wait().is_ok());
-    let df = match fs::read_to_string("Dockerfile") {
-        Ok(s) => s,
-        Err(e) => panic!("Read Dockerfile failed: {}", e),
-    };
-    assert!(df.contains("quay.io/roche/default:1.0.0"));
-    assert!(df.contains("quay.io/roche/alpine:3.12"));
-
-    remove_dir_all(path).unwrap();
-}
 
 #[test]
 fn generate_docker_file_with_params() {
@@ -64,6 +33,7 @@ fn generate_docker_file_with_params() {
         .spawn()
         .unwrap();
     assert!(init_cmd.wait().is_ok());
+
     let df = match fs::read_to_string("Dockerfile") {
         Ok(s) => s,
         Err(e) => panic!("Read Dockerfile failed: {}", e),
